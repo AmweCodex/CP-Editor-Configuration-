57bab674d66d68c1922d26bbe2a715b7939b292a
AmweCodex <134791541+AmweCodex@users.noreply.github.com>
2024-09-08 04:44:16 +0200

    Create Template_V1.cpp

---
+++ Template/Template_V1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+    void solve () {
+        // Write your code here.
+    }
+};
+
+int main () {
+    ios::sync_with_stdio (0);
+    cin.tie (0);
+
+    Solution sol;
+    sol.solve ();
+
+    return 0;
+}
